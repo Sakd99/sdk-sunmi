5835695dcaee5f5901625fa3a507b6fd5c694836
Sakd99 <58140626+Sakd99@users.noreply.github.com>
2024-08-29 11:16:36 +0300

    V1

---
+++ app/build/generated/source/buildConfig/debug/com/sm/sdk/demo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sm.sdk.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sm.sdk.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 260;
+  public static final String VERSION_NAME = "v2.3.20";
+}
